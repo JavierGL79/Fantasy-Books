7e0e9b03789aead0dbd78c6f2281dd2e77b1f32d
JavierGL49 <javiergironlopezbpr@gmail.com>
2024-01-31 06:53:44 +0000

    Funcionalidad BookPage

---
+++ app/Policies/LibroPolicy.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class LibroPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function crearLibro(User $user)
+    {
+        /*
+        crearLibro(User $user)
+        */
+    }
+}
