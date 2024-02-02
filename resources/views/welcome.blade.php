4574c989c34e6da71ee2ceff3cdfc0ef5decf3f0
JavierGL49 <javiergironlopezbpr@gmail.com>
2024-02-02 11:34:58 +0000

    Algunos aspectos visuales.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,34 +1,44 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <h2 class="text-center">Nuestros libros</h2>
-    @if(count($libros) > 0)
-        <ul>
-            @foreach($libros as $libro)
-                <li>
-                    <a href="{{ route('books.BookPage', ['id' => $libro->id]) }}">{{ $libro->titulo }}</a>
-                </li>
-            @endforeach
-        </ul>
-    @else
+<div class="container"  id="notice">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                @if (session('status'))
+                    <div class="card-header">
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    </div>
+                @endif
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="custom-container ml-10 mr-10"  id="books">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><h2 class="text-center">{{__('Our books')}}</h2></div>
+                    <div class="card-body">
+                    @if(count($libros) > 0)
+                        <ul>
+                            @foreach($libros as $libro)
+                            <div class="card card-body">
+                                <li>
+                                    <a href="{{ route('books.BookPage', ['id' => $libro->id]) }}">{{ $libro->titulo }}</a>
+                                </li>
+                            </div>
+                            @endforeach
+                        </ul>
+                    @else
     <div class="alert alert-info text-center">
     {{__('Sorry, no books currently registered')}}.
     </div>
     @endif    
-</div>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
                 </div>
             </div>
