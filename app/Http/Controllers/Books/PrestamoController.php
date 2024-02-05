f8aeaf8fee044cb175022279b936cce9bcf162b8
JavierGL49 <javiergironlopezbpr@gmail.com>
2024-02-05 20:04:02 +0000

    Cread lógica de prestar libro. pendiente la de registrar el prestamo

--- app/Http/Controllers/Books/PrestamoController.php
+++ app/Http/Controllers/Books/PrestamoController.php
@@ -4,8 +4,29 @@ namespace App\Http\Controllers\Books;
 
 use Illuminate\Http\Request;
 use App\Models\Prestamo;
+use App\Events\LibroPrestado;
 
 class PrestamoController extends Controller
 {
-    //
+    public function prestarLibro($libroId)
+    {
+    // Obtener el libro por su ID
+    $libro = Libro::findOrFail($libroId);
+
+    // Verificar si hay stock disponible
+    if ($libro->stock > 0) {
+        
+        $libro->decrement('stock'); // Reducir el stock en 1
+
+        // Registrar el préstamo en tu lógica específica
+        // ...
+
+        // Activar el evento LibroPrestado
+        event(new LibroPrestado());
+
+        return response()->json(['message' => 'Libro prestado exitosamente']);
+    } else {
+        return response()->json(['message' => 'No hay stock disponible para prestar el libro'], 422);
+    }
+    }
 }
