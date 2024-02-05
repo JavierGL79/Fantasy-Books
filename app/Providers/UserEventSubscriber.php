b8a0e3481150b991a3f5ebc35039b468400f574b
JavierGL49 <javiergironlopezbpr@gmail.com>
2024-02-05 18:23:53 +0000

    Creados los eventos UserModificado y UserModificado y añadidos al UserEventSubscriber

--- app/Providers/UserEventSubscriber.php
+++ app/Providers/UserEventSubscriber.php
@@ -32,6 +32,7 @@ class UserEventSubscriber
         return [
             UserCreado::class => 'handleUserCreado',
             UserModificado::class => 'handleUserModificado',
+            UserEliminado::class => 'handleUserModificado'
         ];
         }
 }
