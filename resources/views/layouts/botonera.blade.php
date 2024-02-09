18174c4bb9999886e9397e489f8741f8947000a9
JavierGL49 <javiergironlopezbpr@gmail.com>
2024-02-09 19:12:40 +0000

    Creado botón Eliminar Ususario en la botonera para que sólo lo vea el bibliotecario. No funcional.

--- resources/views/layouts/botonera.blade.php
+++ resources/views/layouts/botonera.blade.php
@@ -1,29 +1,30 @@
 <div class="row mb-0">
     <div class="col-md-6 offset-md-4 text-center" id="Botonera"> 
         @guest
-            <!-- Mostrar el botón de volver para usuarios no autenticados -->
+            <!-- Muestra el botón de volver para usuarios no autenticados -->
             <a href="#" onclick="history.back(); return false;" class="btn btn-danger" id="cancel">{{__('Back')}}</a>
         @else
-            <!-- Mostrar el botón de "Préstame el libro!" para usuarios autenticados y en la página correcta -->
+            <!-- Muestra el botón de "Préstame el libro!" para usuarios autenticados-->
             @if(request()->is('books/*/edit') == false && isset($libro) && isset($libro->stock) && $libro->stock > 0)
                 <button class="btn btn-secondary" id="prestarLibro" data-libro-id="{{ $libro->id }}">{{__('Préstame el libro!')}}</button>
             @endif
 
-            <!-- Mostrar el botón accesible para los bibliotecarios -->
+            <!-- Muestra el botón accesible para los bibliotecarios -->
             @if(Auth::user()->es_bibliotecario)
                 @can('editBook', $libro)
                     <form action="{{ route('books.EditBook', ['id' => $libro->id]) }}" method="GET">
                         @csrf
                         <button type="submit" class="btn btn-primary" id="apply">{{__($buttonText) }}</button>
                     </form>
+                    
+                    <!-- Botón de eliminar -->
+                    @if(request()->is('books/*'))
+                        <button class="btn btn-danger" id="delete" onclick="confirmDelete()">{{__('Delete Book')}}</button>
+                    @elseif(request()->is('profile'))
+                    <button class="btn btn-danger" id="delete" onclick="confirmDelete()">{{__('Delete User')}}</button>
+                        @endif
                 @endcan
-                <!--<button type="submit" class="btn btn-primary" id="aceptar">{{__($buttonText) }}</button>-->
                 
-                <!-- Mostrar el botón de reset solo en la página de edición de libros -->
-                @if(request()->is('books/*/edit'))
-                    <!-- Agrega aquí el botón de reset si es necesario -->
-                @endif
-
             @endif
 
             <!-- Mostrar siempre el botón de volver para usuarios autenticados -->
