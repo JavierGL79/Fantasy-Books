e28762854a027a276c873b43ebfb91890a45ad0d
JavierGL49 <javiergironlopezbpr@gmail.com>
2024-02-09 05:17:54 +0000

    Mostrar dacos en vista EditBook, ok

--- resources/views/books/EditBook.blade.php
+++ resources/views/books/EditBook.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Edit Book') }}</div>
+                <div class="card-header text-center">
+                    <h1>{{ __('Edit Book') }}</h1>
+                </div>
 
                 @if(isset($status) && $status === 'success')
                     <div class="alert alert-success">
@@ -25,39 +27,39 @@
                             <div class="row mb-3">
                                 <label for="autor">Autor:</label>
                                 <div class="col-md-6">
-                                    <input type="text" name="autor" required>
+                                    <input type="text" name="autor" value="{{ $libro->autor }}" required>
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="titulo">Título:</label>
                                 <div class="col-md-6">
-                                    <input type="text" name="titulo" required>
+                                    <input type="text" name="titulo" value="{{ $libro->titulo }}" required>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <label for="year">Año:</label>
                                 <div class="col-md-6">
-                                    <input type="number" name="year" required>
+                                    <input type="number" name="year" value="{{ $libro->year }}" required>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <label for="editorial">Editorial:</label>
                                 <div class="col-md-6">
-                                    <input type="text" name="editorial" required>
+                                    <input type="text" name="editorial" value="{{ $libro->editorial }}" required>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <label for="stock">Stock:</label>
                                 <div class="col-md-6">
-                                    <input type="number" name="stock">
+                                    <input type="number" name="stock" value="{{ $libro->stock }}">
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="foto">Foto:</label>
                                 <div class="col-md-6">
-                                    <input type="file" name="foto">
+                                    <input type="file" name="foto" value="{{ $libro->foto }}">
                                 </div>
                             </div>
                         </div>
@@ -65,7 +67,7 @@
                             <div class="row mb-3">
                                 <label for="information">{{__('Information')}}:</label>
                                 <div class="col-md-6">
-                                    <textarea name="information" rows="4"></textarea>
+                                    <textarea name="information" rows="4"value="{{ $libro->information }}"></textarea>
                                 </div>
                             </div>
                         </div>
