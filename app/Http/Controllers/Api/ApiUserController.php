e65ea2342307540e59c2ff1375ec3fe49569a3ea
JavierGL49 <javiergironlopezbpr@gmail.com>
2024-02-14 06:22:20 +0000

    Creadas collections para el APIRest y un ApiUserController

---
+++ app/Http/Controllers/Api/ApiUserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserCollection;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ApiUserController extends Controller
+{
+    public function index()
+    {
+        $user = User::all();
+        return $user;
+        
+    }
+
+}
--- app/Http/Controllers/Books/PrestamoController.php
+++ app/Http/Controllers/Books/PrestamoController.php
@@ -9,32 +9,34 @@ use App\Events\LibroPrestado;
 
 class PrestamoController extends Controller
 {
-    public function prestarLibro($libroId)
+    public function prestarLibro($id)
     {
-    // Obtener el libro por su ID
-    $libro = Libro::findOrFail($libroId);
+        try {
+            // Obtener el libro por su ID
+            $book = Books::findOrFail($id);
 
-    // Verificar si hay stock disponible
-    if ($libro->stock > 0) {
-        
-        $libro->decrement('stock'); // Reducir el stock en 1
+            // Verificar si hay stock disponible
+            if ($book->stock > 0) {
+                $book->decrement('stock'); // Reducir el stock en 1
 
-        // Registrar el préstamo en tu lógica
-        
-        $prestamo = new Prestamo([
-            'libro_id' => $libro->id,
-            'usuario_id' => auth()->id(), // ID del usuario autenticado
-            'fecha_prestamo' => now(),           
-        ]);
+                // Registrar el préstamo
+                $prestamo = new Prestamo([
+                    'book_id' => $book->id,
+                    'user_id' => auth()->id(), // ID del usuario autenticado
+                    'fecha_prestamo' => now(),
+                ]);
 
-        $prestamo->save();
+                $prestamo->save();
 
-        // Activar el evento LibroPrestado
-        event(new LibroPrestado());
-
-        return response()->json(['message' => 'Libro prestado exitosamente']);
-    } else {
-        return response()->json(['message' => 'No hay stock disponible para prestar el libro'], 422);
-    }
+                // Mensaje de éxito
+                return response()->json(['message' => 'Libro prestado exitosamente'], 200);
+            } else {
+                // Mensaje de error si no hay stock disponible
+                return response()->json(['message' => 'No hay stock disponible para prestar el libro'], 422);
+            }
+        } catch (\Exception $e) {
+            // Capturar excepciones y devolver un mensaje de error
+            return response()->json(['message' => 'Error al prestar el libro: ' . $e->getMessage()], 500);
+        }
     }
 }
