841442e88d8e067c4eb475175dc5660d4557fa41
JavierGL49 <javiergironlopezbpr@gmail.com>
2024-02-15 06:36:04 +0000

    Listado de libros AllLoans Ok

--- resources/views/books/AllLoans.blade.php
+++ resources/views/books/AllLoans.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<p>LISTADO DE LIBROS</p>
+<ul>
+    @foreach ($prestamos as $prestamo)
+    <li>
+        <strong>ID del Préstamo:</strong> {{ $prestamo->id }}<br>
+        <strong>Nombre del Usuario:</strong> {{ $prestamo->user->name }}<br>
+        <strong>Nombre del Libro:</strong> {{ $prestamo->book->titulo }}<br>
+        <!-- Añade aquí más datos que quieras mostrar -->
+    </li>
+    @endforeach
+</ul>
 @endsection
